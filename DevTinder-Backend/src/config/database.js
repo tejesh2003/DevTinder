c5dec6ac27ab246d7a545394135bd28390dcbe1b
tejesh2003 <tejeshchintada22@gmail.com>
2025-03-10 18:23:08 +0530

    Database Credentials hidden

--- DevTinder-Backend/src/config/database.js
+++ DevTinder-Backend/src/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+const dbURI = process.env.DB_STRING;
 
 const ConnectDb = async () =>{
-    await mongoose.connect("mongodb+srv://tejesh:Tejesh%406328@cluster0.eqdpg.mongodb.net/DevTinder");
+    await mongoose.connect(dbURI);
 };
 
 module.exports = ConnectDb;
