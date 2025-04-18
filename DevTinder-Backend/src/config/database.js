d16366d01db69f4455d1726d88f5238f4448a631
tejesh2003 <tejeshchintada22@gmail.com>
2025-04-18 20:44:09 +0530

    Code shift starting

--- DevTinder-Backend/src/config/database.js
+++ DevTinder-Backend/src/config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
-const dbURI = process.env.DB_STRING;
+require("dotenv").config();
+// const dbURI = process.env.DB_STRING;
 
-const ConnectDb = async () =>{
-    await mongoose.connect(dbURI);
+const ConnectDb = async () => {
+  await mongoose.connect(
+    "mongodb+srv://tejesh:Tejesh%406328@cluster0.eqdpg.mongodb.net/DevTinder"
+  );
 };
 
 module.exports = ConnectDb;
